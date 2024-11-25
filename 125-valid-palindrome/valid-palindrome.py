08d38bdcc31580cc458bbfba937cdb2c515075bf
Mohammad Sofyan Abdullah <sofyanrajpoot567@gmail.com>
2024-11-25 20:32:27 +0500

    Time: 18 ms (43.81%) | Memory: 13.9 MB (31.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -7,35 +7,35 @@ class Solution(object):
 
         # Solution 1
 
-        s_new = ""
-        # n = ' '
-        # if n == ' ':
-        #     print('true')
+#         s_new = ""
+#         # n = ' '
+#         # if n == ' ':
+#         #     print('true')
 
-        for i in s:
-            print(i)
-            if i.isdigit() | i.isalpha():
-                print(i)
-                s_new = s_new + i
-            elif i == ' ':
-                continue
-            else:
-                continue
+#         for i in s:
+#             print(i)
+#             if i.isdigit() | i.isalpha():
+#                 print(i)
+#                 s_new = s_new + i
+#             elif i == ' ':
+#                 continue
+#             else:
+#                 continue
 
-        s_new = s_new.lower()
-        reverse_s_new = s_new[::-1]
-        print(reverse_s_new)
-        print(s_new)
+#         s_new = s_new.lower()
+#         reverse_s_new = s_new[::-1]
+#         print(reverse_s_new)
+#         print(s_new)
 
-        if s_new == reverse_s_new:
-            return True
-        else:
-            return False
+#         if s_new == reverse_s_new:
+#             return True
+#         else:
+#             return False
 
-        #Solution 2
+        # Solution 2
 
-        # filtered = ''.join(char.lower() for char in s if char.isalnum())
+        s_new = ''.join(i.lower() for i in s if i.isalnum())
     
-        # return filtered == filtered[::-1]
+        return s_new == s_new[::-1]
 
         
