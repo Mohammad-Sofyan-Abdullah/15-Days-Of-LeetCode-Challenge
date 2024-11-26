7dd9d4caf871edc57d555ae91595c90efed2facf
Mohammad Sofyan Abdullah <sofyanrajpoot567@gmail.com>
2024-11-26 21:14:23 +0500

    Time: 0 ms (100.00%) | Memory: 12.2 MB (6.53%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        # for i in range(0,len(s),2*k):
+        #     sub = s[i:i+k]
+        #     print (sub)
+        #     sub = sub[::-1]
+        #     s = s[:i]+sub+s[i+k:]
+        # return s
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+        
+        # print(s)
+        s = "".join(s)
+        print(s)
+        return s
+        
+            
