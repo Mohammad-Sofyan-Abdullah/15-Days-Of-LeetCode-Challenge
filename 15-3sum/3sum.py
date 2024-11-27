bcbcd30262c9a6b5667218f07206f4b2fdd828a1
Mohammad Sofyan Abdullah <sofyanrajpoot567@gmail.com>
2024-11-27 21:33:53 +0500

    Time: 415 ms (99.10%) | Memory: 20.7 MB (5.08%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -29,61 +29,106 @@ class Solution(object):
         
 #         return unique_list
 
-        solution = list()
-        zeroCounter = 0
+    #Solution 2
+#         solution = list()
+#         zeroCounter = 0
         
-        for num in nums:
-            if num == 0:
-                zeroCounter = zeroCounter + 1
+#         for num in nums:
+#             if num == 0:
+#                 zeroCounter = zeroCounter + 1
             
-        print(zeroCounter)
-        if zeroCounter >= 3:
-            solution.append([0, 0, 0])
+#         print(zeroCounter)
+#         if zeroCounter >= 3:
+#             solution.append([0, 0, 0])
             
-        if 0 in nums:
-            for num in nums:
-                if num != 0:
-                    if -1 * num in nums:
-                        solution.append(sorted([-num, 0, num]))
+#         if 0 in nums:
+#             for num in nums:
+#                 if num != 0:
+#                     if -1 * num in nums:
+#                         solution.append(sorted([-num, 0, num]))
                     
-        n, p = [], []
+#         n, p = [], []
         
-        for i in nums:
-            if i > 0:
-                p.append(i)
-            elif i < 0:
-                n.append(i)
+#         for i in nums:
+#             if i > 0:
+#                 p.append(i)
+#             elif i < 0:
+#                 n.append(i)
         
-        P, N = set(p), set(n)
+#         P, N = set(p), set(n)
     
-        for i in range(len(n)):
-            for j in range(i + 1, len(n)): 
-                x, y = n[i], n[j]
-                target = -1 * (x + y)
-                if target in P:
-                    solution.append(tuple(sorted([x, y, target])))
+#         for i in range(len(n)):
+#             for j in range(i + 1, len(n)): 
+#                 x, y = n[i], n[j]
+#                 target = -1 * (x + y)
+#                 if target in P:
+#                     solution.append(tuple(sorted([x, y, target])))
 
         
-        for i in range(len(p)):
-            for j in range(i + 1, len(p)):
-                x, y = p[i], p[j]
-                target = -1 * (x + y)
-                if target in N:
-                    solution.append(tuple(sorted([x, y, target])))
+#         for i in range(len(p)):
+#             for j in range(i + 1, len(p)):
+#                 x, y = p[i], p[j]
+#                 target = -1 * (x + y)
+#                 if target in N:
+#                     solution.append(tuple(sorted([x, y, target])))
 
-        solution =  [list(x) for x in solution]
+#         solution =  [list(x) for x in solution]
         
-        unique_list = [list(x) for x in set(tuple(x) for x in solution)]
+#         unique_list = [list(x) for x in set(tuple(x) for x in solution)]
 
-        print(unique_list)
+#         print(unique_list)
         
         
-        return unique_list
+#         return unique_list
 
+        if len(nums) < 3:
+            return []
 
-            
-            
-            
+        res = set()
+
+        # 1. Split nums into three lists: negative numbers, positive numbers, and zeros
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0:
+                n.append(num)
+            else:
+                z.append(num)
+
+        # 2. Create a separate set for negatives and positives for O(1) look-up times
+        N, P = set(n), set(p)
+
+        # 3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
+        #   i.e. (-3, 0, 3) = 0
+        if z:
+            for num in P:
+                if -1 * num in N:
+                    res.add((-1 * num, 0, num))
+
+            # 3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
+            if len(z) >= 3:
+                res.add((0, 0, 0))
+
+        # 4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
+        #   exists in the positive number set
+        from itertools import combinations
+
+        for x, y in combinations(n, 2):
+            target = -1 * (x + y)
+            if target in P:
+                res.add(tuple(sorted([x, y, target])))
+
+        # 5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
+        #   exists in the negative number set
+
+        for x, y in combinations(p, 2):
+            target = -1 * (x + y)
+            if target in N:
+                res.add(tuple(sorted([x, y, target])))
+
+        return [list(x) for x in res]
+
             
             
             
