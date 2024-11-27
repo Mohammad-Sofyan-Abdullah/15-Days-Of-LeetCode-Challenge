7bd71d7888b7854178c47bef9109eed241b4bfb7
Mohammad Sofyan Abdullah <sofyanrajpoot567@gmail.com>
2024-11-27 20:38:48 +0500

    Time: 0 ms (100.00%) | Memory: 12.2 MB (9.95%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,11 +5,15 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        if not needle:
-            return 0
+        #Solution 1
+#         if not needle:
+#             return 0
     
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i:i+len(needle)] == needle:
-                return i
+#         for i in range(len(haystack) - len(needle) + 1):
+#             if haystack[i:i+len(needle)] == needle:
+#                 return i
 
-        return -1
+#         return -1
+
+        #Solution 2 One Liner 
+        return haystack.find(needle)
