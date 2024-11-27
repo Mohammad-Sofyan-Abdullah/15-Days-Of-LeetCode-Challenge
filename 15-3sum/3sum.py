04404ca3ed20e2f065a1a3b94c9c9253503b4434
Mohammad Sofyan Abdullah <sofyanrajpoot567@gmail.com>
2024-11-27 21:41:53 +0500

    Time: 464 ms (98.40%) | Memory: 21 MB (5.08%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -29,7 +29,7 @@ class Solution(object):
         
 #         return unique_list
 
-    #Solution 2
+#     Solution 2
 #         solution = list()
 #         zeroCounter = 0
         
@@ -81,12 +81,12 @@ class Solution(object):
         
 #         return unique_list
 
+    #Solution 3 Took Help from the net
         if len(nums) < 3:
             return []
 
         res = set()
 
-        # 1. Split nums into three lists: negative numbers, positive numbers, and zeros
         n, p, z = [], [], []
         for num in nums:
             if num > 0:
@@ -96,22 +96,16 @@ class Solution(object):
             else:
                 z.append(num)
 
-        # 2. Create a separate set for negatives and positives for O(1) look-up times
         N, P = set(n), set(p)
 
-        # 3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
-        #   i.e. (-3, 0, 3) = 0
         if z:
             for num in P:
                 if -1 * num in N:
                     res.add((-1 * num, 0, num))
 
-            # 3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
             if len(z) >= 3:
                 res.add((0, 0, 0))
 
-        # 4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
-        #   exists in the positive number set
         from itertools import combinations
 
         for x, y in combinations(n, 2):
@@ -119,8 +113,6 @@ class Solution(object):
             if target in P:
                 res.add(tuple(sorted([x, y, target])))
 
-        # 5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
-        #   exists in the negative number set
 
         for x, y in combinations(p, 2):
             target = -1 * (x + y)
